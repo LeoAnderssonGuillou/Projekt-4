27950c938ff3e606586021d97f04bdb5703c8817
LeoAnderssonGuillou <leo.anderssonguillou@elev.ga.ntig.se>
2021-04-16 16:28:06 +0200

    Started

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-//TEST
+
+function boardSetup(board) {
+  board.style.display = "grid";
+  board.style.gridTemplateColumns = new Array(10)
+    .fill("10%")
+    .join(" ");
+  board.style.gridTemplateRows = new Array(10).fill("10%").join(" ");
+}
