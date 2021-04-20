841603ac38ce89135acd4c3e34ff812cc35da8d9
LeoAnderssonGuillou <leo.anderssonguillou@elev.ga.ntig.se>
2021-04-20 14:49:25 +0200

    Board + shop start

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
 
-function boardSetup(board) {
-  board.style.display = "grid";
-  board.style.gridTemplateColumns = new Array(10)
-    .fill("10%")
+
+
+
+let grid = new Array(8).fill(0).map(_ => new Array(6).fill({}));
+let shop = new Array(10)
+
+const field = document.querySelector(".field");
+const sidebar = document.querySelector(".sidebar");
+
+//Creates CSS for the main field
+function fieldSetup() {
+  field.style.display = "grid";
+  field.style.gridTemplateColumns = new Array(8)
+    .fill("1fr")
     .join(" ");
-  board.style.gridTemplateRows = new Array(10).fill("10%").join(" ");
+  field.style.gridTemplateRows = new Array(6).fill("1fr").join(" ");
 }
+
+//Creates cells for the main field
+function gridSetup() {
+  for (let y = 0; y < 6; y++) {
+    for (let x = 0; x < 8; x++) {
+      const cell = document.createElement("div");
+      field.appendChild(cell);
+  
+      cell.id = `${x}:${y}`;
+      grid[x][y] = cell.id;
+      // cell.innerHTML = `${x}.${y}`;
+    }
+  }
+}
+
+//Fills the sidebar shop with items
+function shopSetup() {
+  const farm = document.createElement("div");
+  farm.innerHTML = "Farm";
+  farm.addEventListener("click", () => {
+    clickShopItem(0);
+  });
+  shop[0] = farm;
+  sidebar.appendChild(farm);
+
+}
+
+function clickShopItem(x) {
+  shop[x].style.backgroundColor = "rgb(145, 145, 145)";
+}
+
+
+fieldSetup();
+gridSetup();
+shopSetup();
