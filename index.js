eae0564899c5a99564c6871390b255df2535f529
LeoAnderssonGuillou <leo.anderssonguillou@elev.ga.ntig.se>
2021-04-27 14:17:19 +0200

    Updated shop system

--- index.js
+++ index.js
@@ -3,9 +3,12 @@
 
 
 let grid = new Array(8).fill(0).map(_ => new Array(6).fill({}));
-let shop = new Array(10)
+let shop = new Array(1);
 
 let buyState = -1;
+
+const shopColorOff = "rgb(172, 172, 172)";
+const shopColorOn = "rgb(145, 145, 145)";
 
 const field = document.querySelector(".field");
 const sidebar = document.querySelector(".sidebar");
@@ -23,14 +26,17 @@ function fieldSetup() {
 function gridSetup() {
   for (let y = 0; y < 6; y++) {
     for (let x = 0; x < 8; x++) {
-      const cell = document.createElement("div");
-      field.appendChild(cell);
-      cell.addEventListener("click", () => {
+      let cell = {
+        element: document.createElement("div"),
+        inUse: false,
+      };
+      field.appendChild(cell.element);
+      cell.element.addEventListener("click", () => {
         clickField(x, y);
       });
   
-      cell.id = `${x}:${y}`;
-      grid[x][y] = cell.id;
+      cell.element.id = `${x}:${y}`;
+      grid[x][y] = cell;
       // cell.innerHTML = `${x}.${y}`;
     }
   }
@@ -54,31 +60,45 @@ function shopSetup() {
 //When a shop item is clicked on
 function clickShopItem(x) {
   if (shop[x].active == false) {
-    shop[x].element.style.backgroundColor = "rgb(145, 145, 145)";
-    buyState = x;
-    shop[x].active = true;
+    updateShop(x);
   }
   else {
-    shop[x].element.style.backgroundColor = "rgb(172, 172, 172)";
-    buyState = -1;
-    shop[x].active = false;
+    updateShop(-1);
   }
   
 }
 
 //When a field cell is clicked on
 function clickField(x, y) {
-  let cell = document.getElementById(grid[x][y]);
-  switch (buyState) {
-    case -1:
-      console.log("bababooey");
-      break;
-    case 0:
-      const farm = document.createElement("figure");
-      cell.appendChild(farm);
-      
+  let cell = grid[x][y];
+  if (cell.inUse == false) {
+    switch (buyState) {
+      case -1:
+        console.log("bababooey");
+        break;
+      case 0:
+        const farm = document.createElement("figure");
+        cell.element.appendChild(farm);
+        cell.inUse = true;
+        updateShop(-1);
+        
+    }
   }
-  
+}
+
+//Assigns a buyState and updates the status of all shop items accordingly
+function updateShop(state) {
+  buyState = state;
+  for (i = 0; i < shop.length; i++) {
+    if (buyState == i) {
+      shop[i].element.style.backgroundColor = shopColorOn;
+      shop[i].active = true;
+    }
+    else {
+      shop[i].element.style.backgroundColor = shopColorOff;
+      shop[i].active = false;
+    }
+  }
 }
 
 
